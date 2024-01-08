29253d20acd46fcfac3d2ec3f14ae27c2ee3734b
KurtiDLX <marikgames12@gmail.com>
2024-01-08 13:54:55 +0200

    final result

---
+++ src/myPack/Logic.java
@@ -0,0 +1,36 @@
+package myPack;
+
+import java.util.Random;
+
+public class Logic {
+    private String passwordResult;
+    private int random;
+    private char charSet[] = {'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z','1','2','3','4','5','6','7','8','9','0'};
+    private char charGet[];
+
+    public void programLogic()
+    {
+        charGet = new char[getRandom()];
+        for (int i = 0; i < charGet.length; i++){
+            charGet[i] = charSet[getRandom()];
+        }
+        
+        setPasswordResult(new String(charGet));
+        System.out.println(getPasswordResult());
+
+    }
+
+
+    public void setPasswordResult(String passwordResult) {
+        this.passwordResult = passwordResult;
+    }
+
+    public String getPasswordResult(){
+        return passwordResult;
+    }
+
+    public int getRandom(){
+        Random newRandom = new Random();
+       return random = newRandom.nextInt(charSet.length);
+    }
+}
---
+++ src/myPack/Main.java
@@ -0,0 +1,8 @@
+package myPack;
+
+public class Main {
+    public static void main(String[]args) {
+      Logic runner = new Logic();
+      runner.programLogic();
+    }
+}
