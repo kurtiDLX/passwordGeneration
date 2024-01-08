bb1ad63967e0ae55ccdc0905606d947b26218fbb
KurtiDLX <marikgames12@gmail.com>
2024-01-08 14:01:51 +0200

    test new branch

--- src/myPack/Logic.java
+++ src/myPack/Logic.java
@@ -8,13 +8,15 @@ public class Logic {
     private char charSet[] = {'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z','1','2','3','4','5','6','7','8','9','0'};
     private char charGet[];
 
+    //test new branch
+
     public void programLogic()
     {
         charGet = new char[getRandom()];
         for (int i = 0; i < charGet.length; i++){
             charGet[i] = charSet[getRandom()];
         }
-        
+
         setPasswordResult(new String(charGet));
         System.out.println(getPasswordResult());
 
